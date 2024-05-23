12eb19d51af7d80ea66731835e39aa58aafd8ecf
devfabien <ish001fabi@gmail.com>
2024-05-23 12:47:53 +0200

    refactor: update the cached data

--- public/sw.js
+++ public/sw.js
@@ -2,25 +2,30 @@ let cachedData = "appv1";
 this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cachedData).then((cache) => {
-      cache.addAll([
-        "/@vite/client",
-        "/src/main.jsx",
-        "/@react-refresh",
-        "/node_modules/.vite/deps/react_jsx-dev-runtime.js",
-        "/node_modules/.vite/deps/react.js",
-        "/node_modules/.vite/deps/react-dom_client.js",
-        "/src/App.jsx",
-        "/src/index.css",
-        "/src/swDev.js",
-        "/node_modules/.vite/deps/chunk-LHNGGKKR.js",
-        "/node_modules/.vite/deps/chunk-P2LSHJDD.js",
-        "/src/api/fetchWeather.js",
-        "/node_modules/.vite/deps/axios.js",
-        "/bg.jpg",
-        "/logo.jpg",
-        "/index.html",
-        "/",
-      ]);
+      cache
+        .addAll([
+          "/@vite/client",
+          "/src/main.jsx",
+          "/@react-refresh",
+          "/node_modules/.vite/deps/react_jsx-dev-runtime.js",
+          "/node_modules/.vite/deps/react.js",
+          "/node_modules/.vite/deps/react-dom_client.js",
+          "/src/App.jsx",
+          "/src/index.css",
+          "/src/swDev.js",
+          "/node_modules/.vite/deps/chunk-LHNGGKKR.js",
+          "/node_modules/.vite/deps/chunk-P2LSHJDD.js",
+          "/src/api/fetchWeather.js",
+          "/src/hooks/useIsOnline.js",
+          "/node_modules/.vite/deps/axios.js",
+          "/bg.webp",
+          "/logo.png",
+          "/index.html",
+          "/",
+        ])
+        .catch((error) => {
+          console.error("Failed to add resources to cache:", error);
+        });
     })
   );
 });
@@ -43,31 +48,29 @@ this.addEventListener("install", (event) => {
 // });
 
 // Activate event
-// self.addEventListener("activate", function (event) {
-//   event.waitUntil(self.clients.claim());
-// });
+self.addEventListener("activate", function (event) {
+  event.waitUntil(self.clients.claim());
+});
 
-// this.addEventListener("activate", (event) => {
-//   if (navigator.onLine) {
-//     event.waitUntil(
-//       caches.keys().then((keyList) => {
-//         return Promise.all(
-//           keyList.map((key) => {
-//             if (key !== cachedData) {
-//               return caches.delete(key);
-//             }
-//           })
-//         );
-//       })
-//     );
-//   }
-// });
+this.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cachedData) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
 
 // Fetch event
 this.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request.clone());
+      return response || fetch(event.request);
     })
   );
 });
