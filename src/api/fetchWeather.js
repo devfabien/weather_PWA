4540c199e8961335f7417eb7bf81bd82c9c7b2d0
devfabien <ish001fabi@gmail.com>
2024-05-23 12:03:52 +0200

    feat: implement error handling

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 export default async function fetchWeather(query) {
-  const { data } = await axios.get(import.meta.env.VITE_URL, {
-    params: {
-      q: query,
-      units: "metrics",
-      appid: import.meta.env.VITE_APIKEY,
-    },
-  });
+  try {
+    const { data } = await axios.get(import.meta.env.VITE_URL, {
+      params: {
+        q: query,
+        units: "metrics",
+        appid: import.meta.env.VITE_APIKEY,
+      },
+    });
 
-  return data;
+    return data;
+  } catch (error) {
+    if (error.response && error.response.status === 404) {
+      throw new Error("City not found");
+    }
+    throw error;
+  }
 }
