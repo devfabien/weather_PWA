d5dd5ca60efbd5af25df665a7cccaccfeba54b14
devfabien <ish001fabi@gmail.com>
2024-05-23 10:22:24 +0200

    feat: integrate with ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import fetchWeather from "./api/fetchWeather";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [mode, setMode] = useState("online");
 
+  const search = async (e) => {
+    e.preventDefault();
+    if (query.trim().length !== 0) {
+      // const data = await fetchWeather(query)
+      await fetchWeather(query)
+        .then((resp) => {
+          setWeather(resp);
+          setMode("online");
+          localStorage.setItem("weather", JSON.stringify(resp));
+        })
+        .catch(() => {
+          setMode("offline");
+          let collection = localStorage.getItem("weather");
+          setWeather(JSON.parse(collection));
+        });
+      console.log(weather);
+      // console.log(data);
+      // setWeather(data);
+      setQuery("");
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="h-screen w-full bg-[url('/bg.jpg')] bg-no-repeat bg-cover items-center justify-center flex flex-col">
+      {mode === "offline" && (
+        <div className="p-3 rounded-lg bg-yellow-200 my-2 absolute left-0 right-0 top-4 mx-4 w-fit justify-center">
+          city is not found or having some issues with the network
+        </div>
+      )}
+
+      <form
+        id="searchform"
+        className="w-[70%] md:w-[40%] p-3 rounded-lg flex items-center bg-white gap-2"
+        onSubmit={search}
+      >
+        <input
+          name="search"
+          className="w-full p-3 rounded-lg"
+          placeholder="City name...."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 hover:bg-slate-400 rounded-lg"
+          onClick={search}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
+      </form>
+      {/* <button className="bg-black p-4 rounded-lg text-white" onClick={search}>
+        search
+      </button> */}
+      {weather.main && (
+        <div className="mt-3 rounded-xl bg-slate-300/70 w-[80%] md:w-[50%] flex flex-col justify-center items-center p-4">
+          <h2>
+            <span className="text-3xl">{weather.name}</span>
+            <sup className="px-3 rounded-md text-xl bg-yellow-500">
+              {weather.sys.country}
+            </sup>
+          </h2>
+          <div className="my-2 text-2xl font-medium">
+            <span>{(Math.round(weather.main.temp) - 273.15).toFixed(1)}</span>{" "}
+            <sup>&deg;c</sup>
+          </div>
+          <div className="my-2">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p className="my-2 text-xl font-bold italic">
+              {weather.weather[0].description}{" "}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
